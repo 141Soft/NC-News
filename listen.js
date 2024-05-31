6d8ff020ed8ab380b43140aeaa194d85c2d63390
Dan <d.mell>
2024-05-31 13:31:26 +0100

    Adds prod changes

--- listen.js
+++ listen.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, () => {
-    console.log('The server is listening on port 9090.')
+app.listen(PORT, () => {
+    console.log('The server is listening on ${PORT}...')
 })
