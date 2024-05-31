d0e5fd8bd830bca8e8b70a201811ca004d05fc10
Dan <d.mell>
2024-05-31 10:31:26 +0100

    Completes additional functionality for api/articles?topic

--- models/articles_models.js
+++ models/articles_models.js
@@ -13,8 +13,18 @@ exports.fetchArticleByID = (id) => {
 }
 
 exports.fetchArticles = (topic) => {
+    const queryValues = [];
+    let queryString = 'SELECT article_id, title, topic, author, created_at, votes, article_img_url FROM articles'
+
+    if(topic) {
+        queryValues.push(topic)
+        queryString += ' WHERE topic = $1'
+    }
+
+    queryString += ' ORDER BY created_at DESC'
+
     return db
-    .query("SELECT article_id, title, topic, author, created_at, votes, article_img_url FROM articles ORDER BY created_at DESC")
+    .query(queryString, queryValues)
     .then(({ rows }) => {
         if(rows.length === 0){
             return Promise.reject({status:404,msg:"No articles found"})
@@ -46,7 +56,11 @@ exports.updateVotes = (article_id, numVotes) => {
         .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
     })
     .then(({ rows }) => {
-        rows[0].votes += numVotes
+        const updatedVotes = rows[0].votes += numVotes
+        return db
+        .query("UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *", [updatedVotes, article_id])
+    })
+    .then(({ rows }) => {
         return rows[0]
     })
 }
